59bbf0b40098b2f2233dce67015d19c27b7db153
Luferat <contato@luferat.net>
2023-11-29 17:56:08 -0300

    fghj

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,5 +1,15 @@
 function myHome() {
     changeTitle();
     console.log('home funciona');
+
+    getAllItems();
+
 }
+
+function getAllItems() {
+
+    console.log("funciona");
+    return false;
+}
+
 $(document).ready(myHome);
